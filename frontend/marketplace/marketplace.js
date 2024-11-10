20dcfee04e13ecbab849880dba6f4bfc2b28f8df
abhiram1206 <abhiramputta12@gmail.com>
2024-11-10 12:49:47 +0530

    10-11-2024

--- frontend/marketplace/marketplace.js
+++ frontend/marketplace/marketplace.js
@@ -158,3 +158,77 @@ function itemDetails(price, quantity) {
     localStorage.setItem('selectedItem', JSON.stringify(itemData));
     return true; // Allow normal link navigation
 }
+
+
+
+
+
+
+// products fetching from backend api
+let productData = []; 
+
+function fetchData() {
+    fetch("http://localhost:3000/product/getProduct")
+        .then(response => response.json())
+        .then(data => {
+            productData = data.data || [];
+            console.log(productData)
+            loadProducts(productData)
+        })
+        .catch(error => console.error('Error fetching product data:', error));
+}
+
+fetchData();
+
+const productDataDisplay = document.getElementById('rightmarketsection');
+
+function loadProducts(data){
+    productDataDisplay.innerHTML = "";
+    data.forEach(p=>{
+        const row = `
+            <div class="vegetable" id="vegetable1">
+                <div class="veg_st">
+                    <h3 class="sale">Sale</h3>
+                    <div class="fav">
+                        <img src="../assets/carbon_favorite.png" alt="">
+                    </div>
+                </div>
+                <img src="http://localhost:3000/${p.productImage}" alt="${p.productImage}">
+                <h3 class="name_prd">${p.Name}</h3>
+                <h3 class="wt">1kg</h3>
+                <div class="farmer">
+                    <h5>By</h5>
+                    <h5 class="blue">Mark Don</h5>
+                </div>
+                <div class="review">
+                    <img src="../assets/Group 187.png" alt="review">
+                </div>
+                <div class="chat">
+                    <div class="no_rev">
+                        <img src="../assets/chat 1.png" alt="number of reviews">
+                        <h5>19 Reviews</h5>
+                    </div>
+                    <div class="cost">
+                        <h4>₹ <span class="price" id="price1">25.33</span></h4>
+                    </div>
+                </div>
+                <div class="quantity" onclick="toggleOptions(event, 25.33, 'options1', 'selected_quantity1')">
+                    <img src="../assets/ant-menu-submenu-arrow-down.png" alt="">
+                    <h4 style="color: rgb(110, 108, 108);">Quantity: <span class="selected_quantity" id="selected_quantity1" style="color: grey; text-decoration: none;">1</span></h4>
+                </div>
+                <ul class="options" id="options1" style="display: none;"></ul>
+                <a href="../item details/item_details.html" style="text-decoration: none;">
+                    <div class="buynow" style="color: white;" onclick="itemDetails(25.33, parseInt(document.getElementById('selected_quantity1').innerText))">
+                        <h3 class="sale buy_now">Buy Now</h3>
+                    </div>
+                </a>
+                <div class="addtocart">
+                    <img src="../assets/Shopping cart.png" alt="add to cart">
+                    <h4><a href="../cart/cart.html" style="color: grey; text-decoration: none;">Add to Cart</a></h4>
+                </div>
+            </div> 
+        `
+
+        productDataDisplay.insertAdjacentHTML('beforeend', row);
+    })
+}
